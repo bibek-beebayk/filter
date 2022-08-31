d5c3f365f899ab44f37df746c064b54297d70dc2
Bibek Gautam <beebayk0001@gmail.com>
2022-08-31 18:00:27 +0545

    Update

--- main/api.py
+++ main/api.py
@@ -34,41 +34,46 @@ class FilterSelectViewSet(viewsets.GenericViewSet):
         return Response(res)
 
 class FilterViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    # filter_data = {
-    #     'tags': {
-    #         1 : False,
-    #         2 : False,
-    #         3 : False,
-    #         4 : False,
-    #         5 : False,
-    #         6 : False,
-    #         7 : False,
-    #         8 : False,
-    #     },
-    #     'genres': {
-    #         1 : False,
-    #         2 : False,
-    #         3 : False,
-    #         4 : False,
-    #         5 : False,
-    #     },
-    #     'countries': {
-    #         1 : True,
-    #         2 : True,
-    #     }
-    # }
+    
+    filter_data = {
+        'tags': {
+            1 : False,
+            2 : False,
+            3 : False,
+            4 : False,
+            5 : False,
+            6 : False,
+            7 : False,
+            8 : False,
+        },
+        'genres': {
+            1 : True,
+            2 : False,
+            3 : False,
+            4 : False,
+            5 : False,
+        },
+        # 'countries': {
+        #     1 : True,
+        #     2 : True,
+        # }
+    }
 
-    # filter_params = {}
-    # for key, value in filter_data.items():
-    #     values = []       
-    #     for k, v in value.items():
-    #         if v:
-    #             values.append(k)
-    #     filter_params[key] = values
+    filter_params = {}
+    for key, value in filter_data.items():
+        values = []       
+        for k, v in value.items():
+            if v:
+                values.append(k)
+        if values:
+            filter_params[key] = values
+
+
     serializer_class = VideoDetailSerializer  
-    filterset_class = VideoFilter
-    # queryset = Video.objects.all().select_related('country').prefetch_related('tags', 'genres').filter(tags__in = filter_params['tags'], genres__in = filter_params['genres'], country__in = filter_params['countries']).distinct()
 
-    queryset = Video.objects.all().select_related('country').prefetch_related('tags', 'genres')
+    # filterset_class = VideoFilter
+    queryset = Video.objects.all().select_related('country').prefetch_related('tags', 'genres').filter(**filter_params).distinct()
+
+    # queryset = Video.objects.all().select_related('country').prefetch_related('tags', 'genres')
     # filterset_fields = ('country', 'genres', 'tags')
 
